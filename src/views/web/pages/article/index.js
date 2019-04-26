ed26ca4483c77694b7ce181d9814b58e246df504
paul wang <wangj@abc-group.cn>
2019-04-26 18:08:27 +0800

    添加文章详情页面，删除测试页面

---
+++ src/views/web/pages/article/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import {withRouter} from "react-router-dom";
+import {translateMarkdown} from '../../../../lib/utils'
+import './index.scss'
+@withRouter
+class ArticleDetail extends Component {
+
+	state = {
+		loading: false,
+		title: null,
+		tagIds: null,
+		categories: null,
+		content: null
+	};
+
+	getArticleById = async id => {
+		if (!id) {
+			return
+		}
+		this.setState({loading: true});
+		let res = await this.$webApi.getArticleById(id);
+		if (res.flags === 'success') {
+			if (res.data) {
+				let { title, tagIds, categories, content } = res.data;
+				content = translateMarkdown(content);
+				this.setState({
+					title, tagIds, categories, content
+				})
+			}
+		}
+		this.setState({loading: false});
+	};
+
+	componentDidMount() {
+		this.getArticleById(this.props.match.params.id - 0);
+	}
+
+	render() {
+		const {title, content} = this.state;
+		return <div className="article-detail">
+			<div className="article-header">
+				<h1>{title}</h1>
+			</div>
+			<div className="description" dangerouslySetInnerHTML={{ __html: content }} />
+		</div>
+	}
+}
+export default ArticleDetail
--- src/views/web/router.js
+++ src/views/web/router.js
@@ -1,13 +1,12 @@
-import Test from './pages/test/index'
 import WebLayout from './compoents/layout/index'
 import Home from './pages/home/index'
+import ArticleDetail from './pages/article/index'
 export default [{
 	path: '/',
 	name: 'app',
 	component: WebLayout,
 	childRoutes: [
 		{ path: '', component: Home },
-		{ path: 'article/:id', component: Home},
-		{ path: 'test', name: 'test', component: Test}
+		{ path: 'article/:id', component: ArticleDetail}
 	]
 }]
