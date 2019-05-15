53c485ba7e5ccbb1f8614078d2eef0e72cd5d9d3
paul wang <paul.wangjie@gmail.com>
2019-05-15 21:48:26 +0800

    解决redux reducer不刷新问题

--- src/redux/index.js
+++ src/redux/index.js
@@ -4,22 +4,20 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import logger from 'redux-logger'
 import {IS_PROD} from "../conf";
 import {
-	setReduxDataForLocal,
+	getReduxStore,
 	syncStoreDataToWebStorage
 } from '../lib/plugins/redux-plugins'
 import rootReducer from './reducer'
 
 let plugins = IS_PROD ? compose(applyMiddleware(thunk)) : compose(composeWithDevTools(applyMiddleware(thunk, logger)));
 
-const store = (initialState = {}) => {
+const store = (initialState = getReduxStore()) => {
 	const store = createStore(rootReducer, initialState, plugins);
 	if (module.hot && !IS_PROD) {
 		console.log('replacing reducer...');
 		const nextRootReducer = require('./reducer').default;
 		store.replaceReducer(nextRootReducer);
 	}
-	//获取缓存中的redux data
-	setReduxDataForLocal(store);
 	//全局监听redux变量更新
 	syncStoreDataToWebStorage(store);
 	return store
