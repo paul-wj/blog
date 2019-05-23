fdac94f635b9c5d24c750ff7b57f91caad117fd1
paul wang <wangj@abc-group.cn>
2019-05-23 16:14:04 +0800

    全局用户登录，用户注册，用户修改弹窗调试完毕

--- src/views/web/compoents/base/auth-modal.js
+++ src/views/web/compoents/base/auth-modal.js
@@ -1,29 +1,26 @@
 import React, {Component} from 'react'
 import {withRouter} from 'react-router-dom'
-import {Modal, Form, Input, Radio} from 'antd'
+import {Modal, Form, Input, Checkbox} from 'antd'
 
 import {connect} from 'react-redux'
 import {openAuthModal, closeAuthModal} from '../../../../redux/common/actions'
-
-
-
+import {login, register, editUser} from "../../../../redux/user/actions";
 
 const CollectionCreateForm = Form.create({
-	name: 'form_in_modal',
 	onFieldsChange(props, changedFields) {
 		props.onChange(changedFields);
 	},
 	mapPropsToFields(props) {
 		//去除事件参数key
-		const fieldKeys = Object.keys(props).filter(key => key !== 'onChange');
+		const fieldKeys = Object.keys(props).filter(key =>  !['onChange', 'formList'].includes(key));
 		//循环生成FormField结构
-		return fieldKeys.reduce((startValue, nextKey) => {
+		return fieldKeys.length ? fieldKeys.reduce((startValue, nextKey) => {
 			startValue[nextKey] = Form.createFormField({
 				...props[nextKey],
 				value: props[nextKey].value
 			});
 			return startValue
-		}, {});
+		}, {}) : {};
 	}
 })(props => {
 	let {formList} = props;
@@ -41,37 +38,146 @@ const CollectionCreateForm = Form.create({
 	)
 });
 
-@connect(state => ({authModalVisible: state.common.authModalVisible, authModalType: state.common.authModalType, userInfo: state.user.userInfo}), {openAuthModal, closeAuthModal})
+const authTypeMap = {
+	login: { title: '用户登录', formKeys: ['email', 'password']},
+	register: {title: '用户注册', formKeys: ['email', 'username', 'password', 'confirm']},
+	editUser: {title: '修改用户信息', formKeys: ['email', 'username', 'oldPassword', 'password', 'confirm']}
+};
+
+const checkboxOptions = [
+	{ label: '用户名', value: 'username' }, { label: '密码', value: 'password' }
+];
+
+@connect(state => ({
+	authModalVisible: state.common.authModalVisible,
+	authModalType: state.common.authModalType,
+	userInfo: state.user.userInfo}), {
+	openAuthModal,
+	closeAuthModal,
+	login,
+	register,
+	editUser})
 @withRouter
 class AuthModal extends Component {
 	state = {
 		fields: {
 			email: {
-				value: '123',
+				value: ''
+			},
+			username: {
+				value: ''
 			},
 			password: {
-				value: '123123'
+				value: ''
 			},
 			confirm: {
-				value: null
+				value: ''
 			},
-			modifier: {
-				value: 'public'
+			oldPassword: {
+				value: ''
 			}
+		},
+		checkboxValues: ['username']
+	};
+
+	componentWillReceiveProps(nextProps) {
+		const {fields} = this.state;
+		const {userInfo} = nextProps;
+		//修改用户信息时塞入用户数据
+		if (userInfo.userId) {
+			this.setState({fields: Object.assign({}, fields, {
+					email: {value: userInfo.email},
+					username: {value: userInfo.username}
+				})})
+		}
+	}
+
+	checkboxChange = values => {
+		if (values.length === 0) {
+			return this.$toast.warning('请至少勾选一项')
+		}
+		this.setState({checkboxValues: values});
+	};
+
+	/**
+	 * 确认密码的验证
+	 */
+	compareToFirstPassword = (rule, value, callback) => {
+		const form = this.formRef;
+		if (value && value !== form.getFieldValue('password')) {
+			callback('Two passwords that you enter is inconsistent!')
+		} else {
+			callback()
 		}
 	};
 
-	getCurrentForm = () => {
-		const fields = this.state.fields;
-		const FormList = [
+	handleCreate = () => {
+		const {authModalType, userInfo} = this.props;
+		const form = this.formRef;
+		form.validateFields(async (err, values) => {
+			if (err) {
+				return;
+			}
+			// console.log('Received values of form: ', values);
+			if (['login', 'register'].includes(authModalType)) {
+				const {email, username, password} = values;
+				const res = await this.props[authModalType](authModalType === 'login' ? {email, password} : {email, password, username});
+				if (res.flags === 'success') {
+					this.closeModel()
+				}
+			} else {
+				const {email, username, password, oldPassword} = values;
+				const res = await this.props[authModalType](userInfo.userId, {email, username, password, oldPassword});
+				if (res.flags === 'success') {
+					this.closeModel()
+				}
+			}
+		});
+	};
+
+	closeModel = () => {
+		const {fields} = this.state;
+		const form = this.formRef;
+		this.props.closeAuthModal();
+		form.resetFields();
+		this.setState({fields: Object.keys(fields).reduce((startValue, nextKey) => {
+				startValue[nextKey] = {value: ''};
+				return startValue
+			}, {})})
+	};
+
+	handleFormChange = changedFields => {
+		this.setState(({ fields }) => ({
+			fields: { ...fields, ...changedFields },
+		}));
+	};
+
+	render() {
+		const { fields, checkboxValues } = this.state;
+		const { authModalVisible, authModalType, userInfo } = this.props;
+		const formList = [
 			{
 				key: 'email',
 				label: '邮箱',
-				el: <Input placeholder="请输入您的邮箱"/>,
+				el: <Input placeholder="请输入您的邮箱" disabled={!!userInfo.email} />,
 				rules: [
 					{ type: 'email', message: 'The input is not valid E-mail!' },
 					{ required: true, message: 'Please input your E-mail!' }
 				]
+			},
+			{
+				key: 'username',
+				label: '用户名',
+				el: <Input placeholder="请输入您的用户名"/>,
+				rules: [
+					{ required: true, message: 'Please input your username!' }
+				]
+			},
+			{
+				key: 'oldPassword',
+				label: '原密码',
+				el: <Input placeholder="请输入原密码" type="password" />,
+				rules: [{ required: true, message: 'oldPassword is required' }]
 			},
 			{
 				key: 'password',
@@ -82,45 +188,39 @@ class AuthModal extends Component {
 			{
 				key: 'confirm',
 				label: '确认密码',
-				el: <Input placeholder="确认密码" type="password" />,
+				el: <Input placeholder="请输入确认密码" type="password" />,
 				rules: [
-					{ required: true, message: 'Please confirm your password!' }
+					{ required: true, message: 'Please confirm your password!' },
+					{ validator: this.compareToFirstPassword}
 				]
 			}
 		];
-		return FormList;
-	};
-
-	handleCreate = () => {
-		const form = this.formRef;
-		form.validateFields((err, values) => {
-			if (err) {
-				return;
+		let formKeys = authModalType ? authTypeMap[authModalType].formKeys : [];
+		const currentFormList = formList.filter(item => {
+			if (authModalType === 'editUser' && checkboxValues.length === 1) {
+				formKeys = checkboxValues.includes('password') ?
+					formKeys.filter(key => !['username'].includes(key)) :
+					formKeys.filter(key => !['oldPassword', 'confirm'].includes(key));
 			}
-			console.log('Received values of form: ', values);
-			// form.resetFields();
+			return formKeys.includes(item.key)
 		});
-	};
-	handleFormChange = changedFields => {
-		this.setState(({ fields }) => ({
-			fields: { ...fields, ...changedFields },
-		}));
-	};
-	render() {
-		const {fields} = this.state;
-		const { authModalVisible, userInfo } = this.props;
-		const FormList = this.getCurrentForm();
+		const modalTitle = authModalType ? authTypeMap[authModalType].title : '';
 		return (
 			<Modal
-				title="Modal"
+				title={modalTitle}
 				maskClosable={false}
 				visible={authModalVisible}
 				onOk={this.handleCreate}
-				onCancel={this.props.closeAuthModal}
+				onCancel={this.closeModel}
 				okText="确认"
 				cancelText="取消">
+				{authModalType === 'editUser' ?
+					(<div className="edit-user-type">
+						<span className="text">选择您要修改的个人信息：</span>
+						<Checkbox.Group options={checkboxOptions} value={checkboxValues} onChange={this.checkboxChange} />
+					</div>) : null}
 				<CollectionCreateForm
-					formList={FormList}
+					formList={currentFormList}
 					{...fields}
 					ref={formRef => this.formRef = formRef}
 					onChange={this.handleFormChange}/>
