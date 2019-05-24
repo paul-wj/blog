8e2e7c7fdaaaf1a5b589c6e2b933bd2c37e367c4
paul wang <wangj@abc-group.cn>
2019-05-24 18:05:28 +0800

    添加全局清除用户信息方法

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,7 @@
 import marked from 'marked'
 import hljs from 'highlight.js'
 import xss from 'xss'
-
+import _redux from '../redux/index'
 // 转化 md 语法为 html
 export const translateMarkdown = plainText => {
 	return marked(xss(plainText), {
@@ -20,3 +20,12 @@ export const translateMarkdown = plainText => {
 };
 
 export const getRandomColor = () => '0123456789abcdef'.split('').reduce((startValue, currentValue, currentIndex, targetArr) => currentIndex > 5 ? startValue : startValue + targetArr[Math.floor(Math.random()*16)], '#');
+
+export const clearGlobalLocalData = () => {
+	const userInfo = _redux.getState().user;
+	if (userInfo.userId) {
+		_redux.dispatch({ type: 'userInfo', payload: {userId: null, email: null, nick: null, username: null, token: null, avatarColor: null}});
+	}
+	localStorage.removeItem('reduxStore');
+	localStorage.removeItem('authorization');
+};
