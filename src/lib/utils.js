9947e21196a536802d9d566969f69ffc2a05e4d4
paul wang <wangj@abc-group.cn>
2019-06-06 17:45:53 +0800

    添加query参数处理

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -19,6 +19,16 @@ export const translateMarkdown = plainText => {
 	})
 };
 
+export const decodeQuery = url => {
+	let params = {};
+	const paramsStr = url.replace(/\.*\?/, ''); // a=1&b=2&c=&d=xxx&e
+	paramsStr.split('&').forEach(v => {
+		const d = v.split('=');
+		if (d[1] && d[0]) params[d[0]] = d[1]
+	});
+	return params
+};
+
 export const getRandomColor = () => '0123456789abcdef'.split('').reduce((startValue, currentValue, currentIndex, targetArr) => currentIndex > 5 ? startValue : startValue + targetArr[Math.floor(Math.random()*16)], '#');
 
 export const clearGlobalLocalData = () => {
