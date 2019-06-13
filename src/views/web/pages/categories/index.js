648e5f3d4a10d23dc07e8c205e52da9f12c4acde
paul wang <wangj@abc-group.cn>
2019-06-13 14:03:35 +0800

    添加目录跳转方法

--- src/views/web/pages/categories/index.js
+++ src/views/web/pages/categories/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {withRouter, Link} from "react-router-dom";
+import {withRouter} from "react-router-dom";
 import {Badge, Tag} from "antd"
 import './index.scss'
 import { connect } from 'react-redux'
@@ -20,7 +20,7 @@ class Categories extends Component{
 		return <div className="categories">
 			<div className="categories--title">Categories</div>
 			<p className="categories--total">The total number of categories is {categoryList.length}</p>
-			{categoryList.map(category => (<Badge key={category.id} count={category.counts}><Tag color={category.color}>{ category.name }</Tag></Badge>))}
+			{categoryList.map(category => (<Badge key={category.id} count={category.counts}><Tag onClick={e => {this.props.history.push(`/category/${category.id}`)}} color={category.color}>{ category.name }</Tag></Badge>))}
 		</div>
 	}
 }
