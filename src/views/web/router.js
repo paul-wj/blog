1ebe057b803079eb836e9aa12c7427b8b7fd0b77
paul wang <wangj@abc-group.cn>
2019-06-13 14:01:41 +0800

    添加标签文章列表和目录文章列表路由

--- src/views/web/router.js
+++ src/views/web/router.js
@@ -3,6 +3,8 @@ import Home from './pages/home/index'
 import ArticleDetail from './pages/article/index'
 import Archives from './pages/archives'
 import Categories from './pages/categories'
+import CategoryDetail from './common/list/index'
+import TagDetail from  './common/list/index'
 export default [{
 	path: '/',
 	name: 'app',
@@ -12,6 +14,8 @@ export default [{
 		{ path: '/', component: Home, type: 10, name: '首页'},
 		{ path: '/archives', component: Archives, type: 10, name: '归档'},
 		{ path: '/categories', component: Categories, type: 10, name: '目录'},
-		{ path: 'article/:id', component: ArticleDetail, type: 20}
+		{ path: 'article/:id', component: ArticleDetail, type: 20},
+		{ path: 'category/:id', component: CategoryDetail, type: 20},
+		{ path: 'tag/:id', component: TagDetail, type: 20}
 	]
 }]
