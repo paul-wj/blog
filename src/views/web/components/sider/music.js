cfe426df8c1da6360c561672b30902ca20b99eb3
paul wang <wangj@abc-group.cn>
2019-09-09 17:39:19 +0800

    music-howl类独立文件抽离

--- src/views/web/components/sider/music.js
+++ src/views/web/components/sider/music.js
@@ -1,167 +1,15 @@
 import React, { Component, Fragment } from 'react'
-import {Card, Slider, message} from 'antd';
-import {Howl, Howler} from 'howler';
-import lodash from 'lodash'
-class MusicHowl {
-	constructor(playlist) {
-		this.playlist = playlist;
-		this.index = 0;
-		this.timer = null;
-		const currentPlay = playlist[this.index];
-		this.name = currentPlay.name;
-		this.author = currentPlay.author;
-		this.picUrl = currentPlay.picUrl;
-		this.endTime = '';
-	}
+import {Card, Slider} from 'antd';
+import PropTypes from "prop-types";
+import {withRouter} from "react-router-dom";
+import MusicHowl from './music-howl'
 
-	static formatTime(time) {
-		if (!time) {
-			return '0:00'
-		}
-		const minutes = Math.floor(time / 60) || 0;
-		const seconds = (time - minutes * 60) || 0;
-		return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-	}
+@withRouter
+class Music extends Component{
+	static propTypes = {
+		className: PropTypes.string
+	};
 
-	static debounceSeek(callbak) {
-		return (lodash.debounce(callbak, 100))();
-	}
-
-	//开始播放
-	play(soundIndex) {
-		const {playlist, index} = this;
-		soundIndex = soundIndex === undefined ? index : soundIndex;
-		const soundData = playlist[soundIndex];
-		//设置当前播放歌曲名称和歌曲作者、歌曲图片
-		this.name = soundData.name;
-		this.author = soundData.author;
-		this.picUrl = soundData.picUrl;
-
-		const self = this;
-		let sound;
-		if (soundData.howl) {
-			sound = soundData.howl;
-		} else {
-			const step = this.step.bind(this);
-			sound = soundData.howl = new Howl({
-				src: [soundData.url],
-				html5: true,
-				onplay: function () {
-					//设置当前歌曲时间长度
-					self.endTime = MusicHowl.formatTime(Math.round(sound.duration()));
-					self.timer = requestAnimationFrame(step);
-					console.log(`${soundData.name}play`)
-				},
-				onload: function () {
-					console.log(`${soundData.name}load`)
-				},
-				onloaderror: function(id, err) {
-					console.log(id, err, 'loading');
-					if (id) {
-						message.error('歌曲加载出错，请切换歌曲');
-					}
-				},
-				onplayerror: function(id, err) {
-					console.log(id, err, 'playing');
-					if (id) {
-						message.error('歌曲播放出错，请切换歌曲');
-					}
-				},
-				onend: function () {
-					self.cancelStep();
-					self.skip('next');
-					console.log(`${soundData.name}end`)
-				},
-				onpause: function () {
-					self.cancelStep();
-					console.log(`${soundData.name}pause`)
-				},
-				onstop: function () {
-					self.cancelStep();
-					console.log(`${soundData.name}stop`)
-				},
-				onseek: function () {
-					self.timer = requestAnimationFrame(step);
-					console.log(`${soundData.name}seek`)
-				}
-			})
-		}
-		this.index = soundIndex;
-		this.isPause = false;
-		sound.play();
-	}
-	//暂停播放
-	pause() {
-		const {playlist, index} = this;
-		const sound = playlist[index].howl;
-		this.isPause = true;
-		sound.pause();
-	}
-	//切换播放
-	skip(direction) {
-		const {index, playlist} = this;
-		const playlistLength = playlist.length;
-		let currentIndex = 0;
-		if (direction === 'prev') {
-			currentIndex = index - 1;
-			currentIndex = currentIndex < 0 ? playlistLength - 1 : currentIndex;
-		} else {
-			currentIndex = index + 1;
-			currentIndex = currentIndex >= playlistLength ? 0 : currentIndex;
-		}
-		this.skipTo(currentIndex);
-	}
-	//跳至指定索引播放
-	skipTo(currentIndex) {
-		const {index, playlist} = this;
-		const currentSound = playlist[index].howl;
-		if (currentSound) {
-			currentSound.stop();
-		}
-		this.play(currentIndex)
-	}
-	//设置当前歌曲播放点
-	seek(per) {
-		MusicHowl.debounceSeek(() => {
-			const {playlist, index} = this;
-			const sound = playlist[index].howl;
-			if (sound.playing()) {
-				const seek = sound.duration() * (per/100);
-				sound.seek(Math.round(seek));
-			}
-		});
-	}
-	step() {
-		const {playlist, index} = this;
-		const sound = playlist[index].howl;
-		const seek = sound.seek() || 0;
-		this.currentTime = MusicHowl.formatTime(Math.round(seek));
-		this.progress = (seek / sound.duration()) * 100 || 0;
-		if (sound.playing()) {
-			this.cancelStep();
-			this.timer = requestAnimationFrame(this.step.bind(this));
-		}
-	}
-
-	static changeVolume(val) {
-		const volumeNum = val/100;
-		Howler.volume(volumeNum);
-	}
-
-	static changeMute(mute) {
-		Howler.mute(mute);
-	}
-
-	cancelStep() {
-		const {timer} = this;
-		if (timer) {
-			cancelAnimationFrame(timer);
-		}
-	}
-}
-
-
-export default class Music extends Component{
 	songContainerScrollTop = 0;
 	state = {
 		songList: [],
@@ -251,11 +99,12 @@ export default class Music extends Component{
 	}
 
 	toggleSongListContainer() {
+		const {className} = this.props;
 		const {songContainerScrollTop} = this;
 		const {isOpen} = this.state;
 		const currentIsOpen = !isOpen;
 		this.setState({isOpen: currentIsOpen}, () => {
-			const songContainerDom = document.querySelector('.song-container');
+			const songContainerDom = className ? document.querySelector(`.${className} .song-container`) : document.querySelector('.song-container');
 			if (currentIsOpen) {
 				songContainerDom.style.display = 'block';
 				songContainerDom.scrollTop = songContainerScrollTop;
@@ -296,24 +145,27 @@ export default class Music extends Component{
 	}
 
 	render(){
+		const {className} = this.props;
 		const {play, skip, skipTo, onProgressChange, toggleSongListContainer, changeVolume, changeMute} = this;
 		const {isPause, name, author, picUrl, progress, currentTime, endTime, isMask, songList, volume, isMute} = this.state;
-		return (<Fragment>{ songList.length ? <Card className="music">
-			<p className="music-title">私人播放器<span onClick={toggleSongListContainer.bind(this)} className="iconfont icon-category" /></p>
-			<p className="music-name">{name}</p>
-			<p className="music-author">{author}</p>
-			{picUrl ? <p className="music-pic"><img src={picUrl} width="100%" alt={name} /></p> : null}
-			<Slider onChange={onProgressChange.bind(this)} style={{margin: '14px 2px 10px'}} value={progress} tipFormatter={null} />
-			<p className="music-time" style={{visibility: currentTime && endTime ? 'visible' : 'hidden'}}><span>{currentTime}</span>/<span>{endTime}</span></p>
-			<div className="music-control">
-				<i onClick={skip.bind(this, 'prev')} className="iconfont icon-left" />
-				<i onClick={play.bind(this)} className={`iconfont ${isPause ? 'icon-bofang' : 'icon-zanting'}`}/>
-				<i onClick={skip.bind(this, 'next')} className="iconfont icon-left right" />
-			</div>
-			<div className="music-control-options">
-				<div className={`iconfont ${!isMute ? 'icon-shengyin' : 'icon-jingyin'}`}>
-					<span className="placeholder" onClick={changeMute.bind(this)} />
-					<Slider onChange={changeVolume.bind(this)} vertical className="voice-container" value={volume} />
+		return (<Fragment>{ songList.length ? <Card className={`music ${className}`}>
+			<div className="music-content">
+				<p className="music-title">私人播放器<span onClick={toggleSongListContainer.bind(this)} className="iconfont icon-category" /></p>
+				<p className="music-name">{name}</p>
+				<p className="music-author">{author}</p>
+				{picUrl ? <p className="music-pic"><img src={picUrl} width="100%" alt={name} /></p> : null}
+				<Slider onChange={onProgressChange.bind(this)} style={{margin: '14px 2px 10px'}} value={progress} tipFormatter={null} />
+				<p className="music-time" style={{visibility: currentTime && endTime ? 'visible' : 'hidden'}}><span>{currentTime}</span>/<span>{endTime}</span></p>
+				<div className="music-control">
+					<i onClick={skip.bind(this, 'prev')} className="iconfont icon-left" />
+					<i onClick={play.bind(this)} className={`iconfont ${isPause ? 'icon-bofang' : 'icon-zanting'}`}/>
+					<i onClick={skip.bind(this, 'next')} className="iconfont icon-left right" />
+				</div>
+				<div className="music-control-options">
+					<div className={`iconfont ${!isMute ? 'icon-shengyin' : 'icon-jingyin'}`}>
+						<span className="placeholder" onClick={changeMute.bind(this)} />
+						<Slider onChange={changeVolume.bind(this)} vertical className="voice-container" value={volume} />
+					</div>
 				</div>
 			</div>
 			<div className="song-container">
@@ -336,3 +188,5 @@ export default class Music extends Component{
 		</Card> : null} </Fragment>)
 	}
 }
+
+export default Music;
