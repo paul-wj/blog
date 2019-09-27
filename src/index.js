a5319072379513a916f126e63465557f1f6ec6bb
paul wang <wangj@abc-group.cn>
2019-09-27 13:33:44 +0800

    添加延迟加载antd.css cdn,解决atnd.css被自定义css覆盖问题，并去除全局引入loadsh

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import webApi from './lib/api.js'
+import  {LazyLoadingCssCdn} from './lib/utils'
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import store from './redux'
@@ -12,19 +13,14 @@ import './static/scss/index.scss'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { message } from 'antd'
-import lodash from 'lodash'
 
-// markdown 高亮
-// import hljs from 'highlight.js'
-// import javascript from 'highlight.js/lib/languages/javascript'
-// import 'highlight.js/styles/atom-one-light.css'
-//
-// hljs.registerLanguage('javascript', javascript);
+//延迟加载antd css cdn
+LazyLoadingCssCdn('https://cdn.bootcss.com/antd/3.20.5/antd.min.css');
+
 
 // 绑定 全局方法$http
 React.Component.prototype.$webApi = webApi;
 React.Component.prototype.$toast = message;
-React.Component.prototype.$lodash = lodash;
 export default ReactDOM.render(
 	<Provider store={store}>
 		<BrowserRouter>
