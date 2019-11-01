a6e645f76fff829dfdad76f90dde3938ac904391
paul wang <wangj@abc-group.cn>
2019-11-01 16:48:16 +0800

    添加notice推送配置（后期改为后台配置，先将就着用）

--- src/conf/index.js
+++ src/conf/index.js
@@ -1,2 +1,16 @@
 export const IS_PROD = process.env.NODE_ENV === 'production';
-export const VERSION = 0.1;
+export const VERSION = 0.12;
+
+export const noticeTypeList = {
+	10: '评论了你的文章',
+	20: '回复了你的评论文章',
+	30: '点赞了你的评论文章',
+	40: '踩了你的评论文章'
+};
+
+export const noticeRouterList = {
+	10: '/article/',
+	20: '/article/',
+	30: '/article/',
+	40: '/article/'
+};
