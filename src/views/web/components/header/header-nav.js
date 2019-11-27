495c8b4923ef94d5bb9bcf3c8b61f119423a6792
paul wang <wangj@abc-group.cn>
2019-11-27 14:03:30 +0800

    头部导航根据页面尺寸控制是否显示文字

--- src/views/web/components/header/header-nav.js
+++ src/views/web/components/header/header-nav.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
 import {Icon, Menu} from 'antd'
 import {Link, withRouter} from 'react-router-dom'
 
+@connect(state => ({
+	appLayoutWidth: state.app.appLayoutWidth
+}))
 @withRouter
 class HeaderNav extends Component {
 
@@ -17,12 +21,13 @@ class HeaderNav extends Component {
 	};
 
 	render() {
-		const {navList, mode} = this.props;
+		const {navList, mode, appLayoutWidth} = this.props;
+		const isExceed_1100 = appLayoutWidth > 1100;
 		return <Menu className="header-menu" selectedKeys={[this.props.location.pathname]} mode={mode}>
-			{navList.map(nav => <Menu.Item key={nav.path}>
+			{navList.map(nav => <Menu.Item title={nav.name} style={{minWidth: isExceed_1100 ? '' : '50px'}} key={nav.path}>
 				<Link to={nav.path}>
-					{nav.iconType ? nav.iconType === 10 ? <Icon type={nav.iconClassName}/> : <i className={`iconfont ${nav.iconClassName}`}/> : null}
-					<span>{nav.name}</span>
+					{nav.iconType ? nav.iconType === 10 ? <Icon style={{marginRight: isExceed_1100 ? '' : 0}} type={nav.iconClassName}/> : <i className={`iconfont ${nav.iconClassName}`}/> : null}
+					{isExceed_1100 ? <span>{nav.name}</span> : null}
 				</Link>
 			</Menu.Item>)}
 		</Menu>
