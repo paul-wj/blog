3fdccd0e0bda4247b018ca5b2da4e77e3e2281d6
paul wang <wangj@abc-group.cn>
2019-12-24 11:34:32 +0800

    单行显示

--- src/views/web/components/footer/index.js
+++ src/views/web/components/footer/index.js
@@ -13,7 +13,7 @@ class Footer extends Component{
 	render() {
 		const {copyrightYear} = this.state;
 		return <footer className="app-footer">
-			<p className="copyright">版权所有 © 2019—{copyrightYear} <a href="/about" rel="noopener noreferrer">汪小二</a> 保留所有权利<br/><a href="http://www.beian.miit.gov.cn/publish/query/indexFirst.action" rel="noopener noreferrer" target="_blank">赣ICP备19005439号</a></p>
+			<p className="copyright">版权所有 © 2019—{copyrightYear} <a href="/about" rel="noopener noreferrer">汪小二</a> 保留所有权利<a className="footer__icp" href="http://www.beian.miit.gov.cn/publish/query/indexFirst.action" rel="noopener noreferrer" target="_blank">赣ICP备19005439号</a></p>
 		</footer>
 	}
 }
