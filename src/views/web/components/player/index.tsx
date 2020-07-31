fd41a076133acd0e547baeb729f6a29ac5834d63
paul wang <wangj@abc-group.cn>
2020-07-31 18:30:37 +0800

    feat：添加音乐播放器组件

---
+++ src/views/web/components/player/index.tsx
@@ -0,0 +1,52 @@
+import React, {FC, ReactElement, useEffect, useLayoutEffect, useState} from "react";
+// @ts-ignore
+import APlayer from 'aplayer';
+import {getSongList} from "../../../../service/user";
+import {SongInfo} from "../../../../types/user";
+import './index.scss';
+
+interface APlayerProps {
+    name: string;
+    artist: string;
+    url: string;
+    cover: string;
+}
+
+export const AppPlayer: FC = (): ReactElement => {
+
+    const [songList, setSongList] = useState<APlayerProps[]>([]);
+
+    const fetchGetSongList = async () => {
+        const {flags, data} = await getSongList();
+        if (flags === 'success') {
+            setSongList(data.map(({name, picUrl, author, url}: SongInfo) => ({
+                name,
+                artist: author,
+                url,
+                cover: picUrl,
+            })));
+        }
+    };
+
+    useEffect(() => {
+        fetchGetSongList();
+    }, []);
+
+    useLayoutEffect(() => {
+        const aPlayer = new APlayer({
+            container: document.getElementById('music-player'),
+            fixed: true,
+            autoplay: true,
+            audio: songList
+        });
+        return () => {
+            aPlayer.destroy();
+        }
+    }, [songList]);
+
+    return (
+        <div id="music-player" />
+    )
+};
+
+export default AppPlayer;
