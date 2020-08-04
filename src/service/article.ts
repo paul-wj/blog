2b6216d869697a6dbef5a505fcfc9363bd77d3de
paul wang <wangj@abc-group.cn>
2020-08-04 17:58:51 +0800

    feat：添加admin端所需接口

--- src/service/article.ts
+++ src/service/article.ts
@@ -1,6 +1,19 @@
 import Request, {FetchResponse, FetchPageListResponse} from '../lib/plugins/request';
-import {TagInfo, CatalogInfo, ArticleInfo, ArticlePageListRequestBody, CommentInfo, CreateArticleCommentReplyRequestBody} from "../types/article";
-import {PageListResponse, RequestPageBody} from "../types/global";
+import {
+    TagInfo,
+    CatalogInfo,
+    ArticleInfo,
+    ArticlePageListRequestBody,
+    CommentInfo,
+    CreateArticleCommentReplyRequestBody,
+    CreateArticleRequestBody,
+    CreateTagRequestBody,
+    CreateCategoryRequestBody
+} from "../types/article";
+import {
+    PageListResponse,
+    RequestPageBody
+} from "../types/global";
 
 export const getArticleAllList = (): FetchResponse<ArticleInfo[]> => {
     return Request.fetch<ArticleInfo[]>('/article/all');
@@ -10,8 +23,32 @@ export const getTagAllList = (): FetchResponse<TagInfo[]> => {
     return Request.fetch<TagInfo[]>('/tag/all');
 };
 
+export const createTag = (data: CreateTagRequestBody): FetchResponse<boolean> => {
+    return Request.fetch<boolean, CreateTagRequestBody>('/tag', data, {method: 'post'});
+};
+
+export const editTag = (id: number, data: CreateTagRequestBody): FetchResponse<boolean> => {
+    return Request.fetch<boolean, CreateTagRequestBody>(`/tag/${id}`, data, {method: 'patch'});
+};
+
+export const deleteTag = (id: number): FetchResponse<boolean> => {
+    return Request.fetch<boolean, null>(`/tag/${id}`, null, {method: 'delete'});
+};
+
 export const getCategoryAllList = (): FetchResponse<CatalogInfo[]> => {
     return Request.fetch<CatalogInfo[]>('/category/all');
+};
+
+export const createCategory = (data: CreateCategoryRequestBody): FetchResponse<boolean> => {
+    return Request.fetch<boolean, CreateCategoryRequestBody>('/category', data, {method: 'post'});
+};
+
+export const editCategory = (id: number, data: CreateCategoryRequestBody): FetchResponse<boolean> => {
+    return Request.fetch<boolean, CreateCategoryRequestBody>(`/category/${id}`, data, {method: 'patch'});
+};
+
+export const deleteCategory = (id: number): FetchResponse<boolean> => {
+    return Request.fetch<boolean, null>(`/category/${id}`, null, {method: 'delete'});
 };
 
 export const getArticlePageList = (data: ArticlePageListRequestBody): FetchPageListResponse<ArticleInfo> => {
@@ -20,6 +57,19 @@ export const getArticlePageList = (data: ArticlePageListRequestBody): FetchPageL
 
 export const getArticleById = (id: string): FetchResponse<ArticleInfo> => {
     return Request.fetch<ArticleInfo>(`/article/${id}`);
+};
+
+
+export const createArticle = (params: CreateArticleRequestBody): FetchResponse<boolean> => {
+    return Request.fetch<boolean, CreateArticleRequestBody>(`/article`, params, {method: 'post'});
+};
+
+export const editArticle = (id: number, params: CreateArticleRequestBody): FetchResponse<boolean> => {
+    return Request.fetch<boolean, CreateArticleRequestBody>(`/article/${id}`, params, {method: 'patch'});
+};
+
+export const deleteArticle = (id: number): FetchResponse<boolean> => {
+    return Request.fetch<boolean, null>(`/article/${id}`, null, {method: 'delete'});
 };
 
 export const getArticleCommentList = (id: string): FetchResponse<CommentInfo[]> => {
@@ -54,4 +104,3 @@ export const getArticlePageListByCategoryId = (id: string, params: RequestPageBo
 export const getArticlePageListByTagId = (id: string, params: RequestPageBody): FetchPageListResponse<ArticleInfo> => {
     return Request.fetch<PageListResponse<ArticleInfo>, RequestPageBody>(`/article/tag/${id}`, params, {method: 'get'});
 };
-
