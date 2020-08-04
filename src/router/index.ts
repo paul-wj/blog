c6ace7913000d8a29472ad8b45a44b9d9ac449c0
paul wang <wangj@abc-group.cn>
2020-08-04 18:01:24 +0800

    feat：添加admin端路由，此次重构全部完成，此次重构技术更新为react hooks写法，react hooks函数式组件和传统class组件相比业务代码层面更清晰，性能方面也有所提升。

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,18 @@
 import React from 'react';
 import {LoadableComponent} from 'react-loadable';
-import {HomeOutlined, EditOutlined, FolderOutlined, FundOutlined, UserOutlined} from '@ant-design/icons';
-import {RouteConfigComponentProps} from "react-router-config";
+import {
+    HomeOutlined,
+    EditOutlined,
+    FolderOutlined,
+    FundOutlined,
+    UserOutlined,
+    ReadOutlined
+} from '@ant-design/icons';
+import {RouteConfig} from "react-router-config";
 import createLoadableBar from '../views/components/lazy';
 
 const Layout = createLoadableBar(() => import(/* webpackChunkName: 'Layout' */'../views/web/components/layout/index'));
+const NotFound  = createLoadableBar(() => import(/* webpackChunkName: 'NotFound' */'../views/components/404'));
 const Home = createLoadableBar(() => import(/* webpackChunkName: 'Home' */'../views/web/pages/home'));
 const Article = createLoadableBar(() => import(/* webpackChunkName: 'Article' */'../views/web/pages/article'));
 const Archives = createLoadableBar(() => import(/* webpackChunkName: 'Archives' */'../views/web/pages/archives'));
@@ -14,20 +22,40 @@ const About = createLoadableBar(() => import(/* webpackChunkName: 'About' */'../
 const catalogDetail = createLoadableBar(() => import(/* webpackChunkName: 'catalogDetail' */'../views/web/pages/catalog/catalogDetail'));
 const TagDetail = createLoadableBar(() => import(/* webpackChunkName: 'TagDetail' */'../views/web/pages/tag/tagDetail'));
 const AdminLayout = createLoadableBar(() => import(/* webpackChunkName: 'AdminLayout' */'../views/admin/components/layout/index'));
+const AdminEmpty = createLoadableBar(() => import(/* webpackChunkName: 'AdminEmpty' */'../views/admin/components/empty/index'));
 const AdminHome = createLoadableBar(() => import(/* webpackChunkName: 'AdminHome' */'../views/admin/pages/home'));
+const AdminArticleList = createLoadableBar(() => import(/* webpackChunkName: 'AdminArticleList' */'../views/admin/pages/article'));
+const AdminArticleEdit = createLoadableBar(() => import(/* webpackChunkName: 'AdminArticleEdit' */'../views/admin/pages/article/articleEdit'));
+const AdminTagList = createLoadableBar(() => import(/* webpackChunkName: 'AdminTagList' */'../views/admin/pages/tag/tagList'));
+const AdminCategoryList = createLoadableBar(() => import(/* webpackChunkName: 'AdminCategoryList' */'../views/admin/pages/tag/categoryList'));
 
-export interface IRouterConfig {
+export interface IRouterConfig extends Omit<RouteConfig, 'component'> {
     component?: LoadableComponent;
-    render?: (props: RouteConfigComponentProps<any>) => React.ReactNode;
-    path?: string;
-    exact?: boolean;
-    name?: string;
     icon?: string | React.ForwardRefExoticComponent<any>;
     isMenu?: boolean;
-    routes?: IRouterConfig[]
 }
 
 const routes: IRouterConfig[] = [
+    {
+        path: '/admin',
+        component: AdminLayout,
+        routes: [
+            {path: '/admin', exact: true, component: AdminHome, name: '首页', icon: HomeOutlined},
+            {
+                path: '/admin/article',
+                component: AdminEmpty,
+                name: '文章管理',
+                icon: ReadOutlined,
+                routes: [
+                    {path: '/admin/article/edit', name: '新增文章', component: AdminArticleEdit},
+                    {path: '/admin/article/list', name: '文章列表', component: AdminArticleList},
+                    {path: '/admin/article/tag-list', name: '标签列表', component: AdminTagList},
+                    {path: '/admin/article/category-list', name: '目录列表', component: AdminCategoryList},
+                ]
+            },
+            {path: '*', component: NotFound}
+        ]
+    },
     {
         path: '/',
         component: Layout,
@@ -40,13 +68,7 @@ const routes: IRouterConfig[] = [
             {path: '/article/:id',component: Article, isMenu: false},
             {path: '/tag/:id', component: TagDetail, isMenu: false},
             {path: '/catalog/:id', component: catalogDetail, isMenu: false},
-        ]
-    },
-    {
-        path: '/',
-        component: AdminLayout,
-        routes: [
-            {path: '/admin', exact: true, component: AdminHome, name: '后台首页'},
+            {path: '*', component: NotFound, isMenu: false}
         ]
     }
 ];
