d9d2731a69864d2a43d9aa40f948583f25f89dff
paul wang <wangj@abc-group.cn>
2020-08-05 15:09:21 +0800

    fix：404和player组件移动至全局组件中，并优化404页面

--- src/views/components/404/index.tsx
+++ src/views/components/404/index.tsx
@@ -1,10 +1,18 @@
 import React, {ReactElement} from 'react';
 import {Empty} from 'antd';
+import {createFromIconfontCN} from '@ant-design/icons';
+import {ICON_FONT_URL} from "../../../conf";
+import './index.scss';
+
+const MyIcon = createFromIconfontCN({
+    scriptUrl: ICON_FONT_URL
+});
 
 const AppNotFound = (): ReactElement => {
     return (
         <Empty
-          image={Empty.PRESENTED_IMAGE_SIMPLE}
+          className="empty-container"
+          image={<MyIcon type="icon-yepian" style={{fontSize: '200px'}} />}
           description="Page Not Found"
         />
     )
---
+++ src/views/components/player/index.tsx
@@ -0,0 +1,51 @@
+import React, {FC, ReactElement, useEffect, useLayoutEffect, useState} from "react";
+// @ts-ignore
+import APlayer from 'aplayer';
+import {getSongList} from "../../../service/user";
+import {SongInfo} from "../../../types/user";
+import './index.scss';
+
+interface APlayerProps {
+    name: string;
+    artist: string;
+    url: string;
+    cover: string;
+}
+
+export const AppPlayer: FC = (): ReactElement => {
+
+    const [songList, setSongList] = useState<APlayerProps[]>([]);
+
+    const fetchGetSongList = async () => {
+        const {flags, data} = await getSongList();
+        if (flags === 'success') {
+            setSongList(data.map(({name, picUrl, author, url}: SongInfo) => ({
+                name,
+                artist: author,
+                url,
+                cover: picUrl,
+            })));
+        }
+    };
+
+    useEffect(() => {
+        fetchGetSongList();
+    }, []);
+
+    useLayoutEffect(() => {
+        const aPlayer = new APlayer({
+            container: document.getElementById('music-player'),
+            fixed: true,
+            audio: songList
+        });
+        return () => {
+            aPlayer.pause();
+        }
+    }, [songList]);
+
+    return (
+        <div id="music-player" />
+    )
+};
+
+export default AppPlayer;
