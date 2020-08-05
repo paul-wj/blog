679dfe56188198b3710f6fe201a7314cddab637e
paul wang <wangj@abc-group.cn>
2020-08-05 15:07:18 +0800

    feat：更新iconfont外链

--- src/conf/index.ts
+++ src/conf/index.ts
@@ -4,7 +4,7 @@ export const BASE_URL = BASE_API_URL;
 export const BASE_SOCKET_URL = SOCKET_URL;
 export const VERSION = '1.0.0';
 export const REACT_STORE_KEY = 'reactStore';
-export const ICON_FONT_URL = '//at.alicdn.com/t/font_1231743_reoorxntai.js';
+export const ICON_FONT_URL = '//at.alicdn.com/t/font_1231743_7b76y610pwe.js';
 export const TOKEN_KEY = 'authorization';
 export const REFRESH_TOKEN_KEY = 'refreshToken';
 
