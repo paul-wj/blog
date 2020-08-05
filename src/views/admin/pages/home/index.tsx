e09be7b0c2148e9d598bb180fd91d77755cc09a0
paul wang <wangj@abc-group.cn>
2020-08-05 15:11:36 +0800

    feat：后台首页内容更新

--- src/views/admin/pages/home/index.tsx
+++ src/views/admin/pages/home/index.tsx
@@ -1,10 +1,31 @@
-import React, {FC, ReactElement} from 'react';
+import React, {FC, ReactElement, useCallback} from 'react';
+import {useMappedState} from "redux-react-hook";
+import UserAvatar from '../../../web/components/userAvatar';
+import './index.scss';
 
 
 const AdminHome: FC = (): ReactElement => {
-  return (
-      <div>AdminHome</div>
-  )
+
+    const userState = useCallback(state => ({userInfo: state.user.userInfo}), ['user']);
+
+    const {userInfo} = useMappedState(userState);
+
+    return (
+        <div className="admin-home">
+            <div className="admin-home__info">
+                <p className="admin-home__title">WELCOME</p>
+                <p className="admin-home__desc">欢迎进入后台系统</p>
+                <UserAvatar
+                  profilePicture={userInfo.profilePicture}
+                  username={userInfo.username}
+                  avatarColor={userInfo.avatarColor}
+                  size={150}
+                />
+                <p className="admin-home__name">{userInfo.username}</p>
+                <p className="admin-home__saying">实践是检验真理的唯一标准</p>
+            </div>
+        </div>
+    )
 };
 
 export default AdminHome;
